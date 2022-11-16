69223c3f3865c354f6fa3e38a81e19c82cb528c5
DeveloperKenny <81117537+DeveloperKenny@users.noreply.github.com>
2022-11-15 19:15:18 -0800

    Changed the entire theme

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar }  from './Components/Navbar/Navbar';
+import { Hero } from './Components/Hero/Hero';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <Navbar />
+    <Hero />
+
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import '../Hero/Hero.css';
+
+
+
+
+
+export const Hero = () => {
+
+
+
+
+    return (
+        <div>
+        <div className="container">
+        <div className="ninja">
+            <div className="hello-text">Hello!</div>
+                <div className="intro-text">My name is <span>Ken</span></div>
+                    <div className="title-text">I am a  
+                    <span>Frontend Developer.</span></div>
+        </div> 
+        </div>   
+        </div>        
+        
+    )
+}
